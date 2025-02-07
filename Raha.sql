999f4bdd52e2e6c717f18af2c85397032b73b032
Raha__bdr <103226302+raha1378@users.noreply.github.com>
2025-02-07 11:33:52 +0330

    eee

--- Raha.sql
+++ Raha.sql
@@ -1,56 +1,8 @@
-# Soal 1
-USE exam;
-SELECT SUM(Quantity*UnitPrice) AS TotalSale
-FROM sale_table ;
- 
-#Soal 2
-USE exam;
-SELECT COUNT(DISTINCT customer) AS TotalCustomer
-FROM sale_table  ;
-
-
-#Soal 3
-SELECT Product,
-	   SUM(Quantity) AS ProductSale
-FROM sale_table
-GROUP BY Product
-ORDER BY Product ASC;
-
-
-#Soal 4
-ALTER TABLE sale_table ADD Total INT;
-UPDATE sale_table SET Total = Quantity*UnitPrice WHERE Total IS NULL;
-SELECT Customer,
-       SUM(total) AS TotalPrice,
-       COUNT(customer) AS FactorNum, 
-       SUM(quantity) AS SaleNum 
-FROM sale_table 
-WHERE Total > 1500 
-GROUP BY Customer
-ORDER BY Customer ASC;
-
-
-#Soal 5
-SELECT SUM(TotalQuantity * ProfitRatio) / 100 AS ProfitPercentage ,
-       SUM(TotalPrice * ProfitRatio) / 100 AS ProfitAmount
-FROM(
-     SELECT S.Product,
-             SUM(S.Quantity) AS TotalQuantity, 
-             SUM(S.Total) AS TotalPrice, 
-             P.ProfitRatio 
-             FROM sale_table AS S INNER JOIN sale_profit AS P 
-                  ON S.Product = P.Product 
-                  GROUP BY Product
-     ) 
-      AS Profits;
-
-
-#Soal 6
-SELECT SUM(CustomerNum) AS TotalCustomers 
-FROM(
-     SELECT COUNT(DISTINCT Customer) AS CustomerNum,
-     Date 
-     FROM sale_table GROUP BY Date
-     ) 
-     AS tbl1;
-
+SELECT customer_key AS 'ID', custmer_customer AS 'Customer name', sum(sales_salesAmount) AS
+    'sale Amount' from customer
+        INNER JOIN sales ON
+            customer.customer_key = sales.sales_customerKey
+        WHERE customer_countryRegion = 'Germany'
+        GROUP BY customer_key
+        ORDER BY SUM(sales_salesAmount) DESC
+        LIMIT 15;
